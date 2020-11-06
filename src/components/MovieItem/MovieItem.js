521667acec2cee1de40487d763d0ecb117d94f44
Khaildoran <sgermscheid@gmail.com>
2020-11-06 16:59:45 -0600

    stying for list complete

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -3,16 +3,21 @@ import './MovieItem.css'
 
 class MovieItem extends Component{
 
-
+    handleItemCardSelect = () => {
+        console.log(this.props);
+        
+    }
     render() {
 
         // creating readable alt tag
         let altInfo = this.props.movieRecord.title + ' Movie poster image'
+        // max char to display
+        let maxDescriptionLength = this.props.movieRecord.description.substring(0,150);
         return(
-           <div className="movieItemWrap" >
+           <div className="movieItemWrap" onClick={this.handleItemCardSelect}>
                 <img src={this.props.movieRecord.poster} alt={altInfo} ></img>
                 <p className="title">{this.props.movieRecord.title}</p>
-                <p className="description">{this.props.movieRecord.description}</p>
+                <p className="description">{maxDescriptionLength} <span className ="endDescText"> . . . click to continue Reading</span> </p>
            </div>
         )
     }
