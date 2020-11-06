c81a2ce9255032365833a1f958611ac2d67b0d8a
Khaildoran <sgermscheid@gmail.com>
2020-11-06 16:41:39 -0600

    styling for movie list cards

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './MovieItem.css'
 
 class MovieItem extends Component{
 
@@ -9,7 +10,9 @@ class MovieItem extends Component{
         let altInfo = this.props.movieRecord.title + ' Movie poster image'
         return(
            <div className="movieItemWrap" >
-               <img src={this.props.movieRecord.poster} alt={altInfo} ></img>
+                <img src={this.props.movieRecord.poster} alt={altInfo} ></img>
+                <p className="title">{this.props.movieRecord.title}</p>
+                <p className="description">{this.props.movieRecord.description}</p>
            </div>
         )
     }
